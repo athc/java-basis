1acb3cbfb6a10c031704088a1fafd295948c1593
dujf <king54732@163.com>
2019-03-11 17:26:53 +0800

    fix : 观察者模式

---
+++ design-parttem/src/org/orion/observer/Boss.java
@@ -0,0 +1,36 @@
+package org.orion.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author <a href="http://github.com/athc">dujf</a>
+ * @date 2019-03-11
+ * @since JDK1.8
+ */
+public class Boss implements Observer {
+  private List<Colleague> colleagues = new ArrayList<>();
+
+  private String message;
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  @Override public void addColleague(Colleague colleague) {
+    colleagues.add(colleague);
+  }
+
+  @Override public void removeColleague(Colleague colleague) {
+    colleagues.remove(colleague);
+  }
+
+  @Override public void notifyColleague() {
+    System.out.println(message);
+    colleagues.forEach(colleague -> colleague.working());
+  }
+}
