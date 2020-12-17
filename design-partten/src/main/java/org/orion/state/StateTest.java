03ab219ddf8097b5214576801da9d82baf81d3b1
dujf <dujf54732@gmail.com>
2020-12-17 11:13:11 +0800

    fix: java 并发实践编程学习

--- design-partten/src/main/java/org/orion/state/StateTest.java
+++ design-partten/src/main/java/org/orion/state/StateTest.java
@@ -1,8 +1,6 @@
 package org.orion.state;
 
 
-import org.junit.Test;
-
 /**
  * @author <a href="http://github.com/athc">dujf</a>
  * @date 2019-03-12
@@ -11,40 +9,39 @@ import org.junit.Test;
  */
 public class StateTest {
 
-  @Test
-  public void testState() {
-    Work work = new Work();
-    work.setHour(9);
-    work.setFinished(false);
-    State state = new MorningState();
-    state.working(work);
+    public static void main(String[] args) {
+        Work work = new Work();
+        work.setHour(9);
+        work.setFinished(false);
+        State state = new MorningState();
+        state.working(work);
 
 
-    work.setHour(12);
-    state = new NoonState();
-    state.working(work);
+        work.setHour(12);
+        state = new NoonState();
+        state.working(work);
 
-    work.setHour(15);
-    state = new AfternoonState();
-    state.working(work);
+        work.setHour(15);
+        state = new AfternoonState();
+        state.working(work);
 
-    work.setHour(17);
-    state = new AfternoonState();
-    state.working(work);
+        work.setHour(17);
+        state = new AfternoonState();
+        state.working(work);
 
-    work.setHour(18);
-    state = new AfternoonState();
-    state.working(work);
+        work.setHour(18);
+        state = new AfternoonState();
+        state.working(work);
 
-    work.setHour(19);
-    state = new NightState();
-    state.working(work);
+        work.setHour(19);
+        state = new NightState();
+        state.working(work);
 
-    work.setHour(20);
-    work.setFinished(true);
-    state = new NightState();
-    state.working(work);
+        work.setHour(20);
+        work.setFinished(true);
+        state = new NightState();
+        state.working(work);
 
-  }
+    }
 
 }
