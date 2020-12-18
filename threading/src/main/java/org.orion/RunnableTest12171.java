5c958d6bbc12058319bcade69ab439c503afd837
dujf <dujf54732@gmail.com>
2020-12-18 10:24:47 +0800

    feat： 等待唤醒

---
+++ threading/src/main/java/org.orion/RunnableTest12171.java
@@ -0,0 +1,62 @@
+package org.orion;
+
+
+import org.junit.Test;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * @author <a href="http://github.com/athc">dujf</a>
+ * @date 2019-03-11
+ * @since JDK1.8
+ */
+public class RunnableTest12171 {
+
+
+    @Test
+    public void test() throws InterruptedException {
+        ConcurrentLinkedQueue<String> bag = new ConcurrentLinkedQueue<String>();
+        Runnable p = () -> {
+            synchronized (this) {
+                while (true) {
+                    bag.add(System.currentTimeMillis() + "");
+                    System.out.println("生产了：" + bag.size());
+                    if (!bag.isEmpty()) {
+                        try {
+                            notifyAll();
+                            wait();
+                            System.out.println("生产唤醒le");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        };
+
+        Runnable c = () -> {
+            synchronized (this) {
+                while (true) {
+                    System.out.println("消费了：" + bag.poll());
+                    if (bag.isEmpty()) {
+                        try {
+                            notifyAll();
+                            wait();
+                            System.out.println("消费唤醒le");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        };
+
+        Thread t = new Thread(p);
+        Thread t2 = new Thread(c);
+        t.start();
+        t2.start();
+        Thread.sleep(1000 * 6);
+    }
+
+
+}
